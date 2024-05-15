45467af8735a1f8f54b3c1e475aa1202aebcb2be
brussi <brussi.giovanni@itismeucci.com>
2024-05-15 13:12:48 +0200

    dovrebbe funzionare

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js';
+import {useState} from 'react';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [partita, setPartita] = useState([]);
+  const [show, setShow] = useState(false);
+  const [start, setStart] = useState(true);
+
+  async function nuovaPartita(){
+    setStart(false);
+    setShow(true);
+    setLoading(true);
+    const response = await fetch("http://localhost:8080/partita", {method: "POST"});
+    const nuovoArray = await response.json();
+    setPartita(nuovoArray);
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Indovina il numero</h1>
+      {
+        start &&
+        <button onClick={nuovaPartita}>Inizia</button>
+      }
+
+
+
+      {show &&
+        <>
+        {loading ?
+          <div>in caricamento...</div>
+        :
+        <Form id={partita.id} numero={partita.numero} tentativi={partita.tentativi}/>
+        }
+        </>
+      }
+     
     </div>
   );
 }
---
+++ app/src/Form.js
@@ -0,0 +1,83 @@
+import {useState} from 'react';
+
+
+export default function Form({id, numero, tentativi}){
+
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(true);
+
+    const [n, setN] = useState("");
+    const [tent, setTent] = useState(tentativi);
+    const [stato, setStato] = useState(-2);
+
+    function controllaNumero(e){
+        setN(e.target.value);
+    }
+
+    async function invia(){
+        setLoading(true);
+        const response = await fetch(`http://localhost:8080/partita/${id}`, {
+            method: "PUT",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({numero: n})
+        });
+        const nuovoArray = await response.json();
+
+        setTent(nuovoArray.tentativi);
+        setStato(nuovoArray.risultato);
+
+        setLoading(false);
+    }
+
+    async function nuovaPartita(){
+        setLoading(true);
+        const response = await fetch("http://localhost:8080/partita", {method: "POST"});
+        const nuovoArray = await response.json();
+        setLoading(false);
+      }
+
+
+    return(
+        <div>
+            <div>
+                <div>
+                    <button onClick={nuovaPartita}>Nuova partita</button>
+                    {
+                        loading ?
+                            <div>in caricamento...</div>
+                        :
+                        <>
+
+                        {stato === 0 &&
+                            <p>Hai indovinato il numero</p>
+                        }
+                        {stato === -1 &&
+                            <p>Il numero è troppo piccolo</p>
+                        }
+                        {stato === 1 &&
+                            <p>Il numero è trooppo grande</p>
+                        }
+                        
+                        {
+                            stato != 0 &&
+                            <>
+                                <p>Tentativi: {tent}</p>
+                                <p>ID: {id}</p>
+                                <p>Inserisci un numero tra 1 e 100</p>
+                                <input type="text" placeholder="Inserisci il numero" value={n} onChange={controllaNumero}></input>
+                            </>
+                        }
+
+                        </>
+                    }
+                    
+                    
+                    
+                </div>
+                <div>
+                    <button onClick={invia}>Invia</button>
+                </div>
+            </div>
+        </div>
+    )
+}
